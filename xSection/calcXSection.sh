ac2a87e40b68f4574d59d6fd83781bd0ec75a1d4
Nathalia Cardona <n.cardonac@uniandes.edu.co>
2020-06-14 18:57:40 -0400

    using set params in mg5 file instead of changing paramcards

--- xSection/calcXSection.sh
+++ xSection/calcXSection.sh
@@ -17,7 +17,7 @@ outputFolder=/home/aflorez/genSignalAndXSection/results/xSection/n2_${n2}_ch1_${
 
 # create mg5 file
 cp ${currentFolder}templates/standardMgFile.mg5 ${currentFolder}currentRun/mgFile.mg5
-cp ${currentFolder}templates/standardParamCard.dat ${currentFolder}currentRun/param_card.dat
+# cp ${currentFolder}templates/standardParamCard.dat ${currentFolder}currentRun/param_card.dat
 
 # create ouput folder
 mkdir -p $outputFolder 
@@ -25,19 +25,26 @@ mkdir -p $outputFolder
 sed -i "s|outputFolder|${outputFolder}|g" ${currentFolder}currentRun/mgFile.mg5
 
 # run mg5_aMC mg5File to create stuff in folder of line 2
-mg5_aMC ${currentFolder}currentRun/mgFile.mg5
+# mg5_aMC ${currentFolder}currentRun/mgFile.mg5
 
 # in own paramcard file, change masses
-sed -i "s/      1000022 5.000000e+01 # mneu1/      1000022 ${n1} # mneu1/g" ${currentFolder}currentRun/param_card.dat
-sed -i "s/      1000023 1.000000e+02 # mneu2/      1000023 ${n2} # mneu2/g" ${currentFolder}currentRun/param_card.dat
-sed -i "s/      1000024 7.500000e+01 # mch1/      1000024 ${ch1} # mch1/g" ${currentFolder}currentRun/param_card.dat
+# sed -i "s/      1000022 5.000000e+01 # mneu1/      1000022 ${n1} # mneu1/g" ${currentFolder}currentRun/param_card.dat
+# sed -i "s/      1000023 1.000000e+02 # mneu2/      1000023 ${n2} # mneu2/g" ${currentFolder}currentRun/param_card.dat
+# sed -i "s/      1000024 7.500000e+01 # mch1/      1000024 ${ch1} # mch1/g" ${currentFolder}currentRun/param_card.dat
 
-cp ${currentFolder}currentRun/param_card.dat ${outputFolder}Cards/param_card.dat
-cp ${currentFolder}templates/run_card.dat ${outputFolder}Cards/run_card.dat
+# cp ${currentFolder}currentRun/param_card.dat ${outputFolder}Cards/param_card_default.dat
+# cp ${currentFolder}templates/run_card.dat ${outputFolder}Cards/run_card_default.dat
 
-echo "launch " >> ${currentFolder}currentRun/mgFile.mg5
-echo "${outputFolder}Cards/param_card.dat" >> ${currentFolder}currentRun/mgFile.mg5
-echo "${outputFolder}Cards/run_card.dat" >> ${currentFolder}currentRun/mgFile.mg5
+# cp ${currentFolder}currentRun/param_card.dat ${outputFolder}Cards/param_card.dat
+# cp ${currentFolder}templates/run_card.dat ${outputFolder}Cards/run_card.dat
+
+echo "launch ${outputFolder}" >> ${currentFolder}currentRun/mgFile.mg5
+echo "set mneu1 ${n1}" >> ${currentFolder}currentRun/mgFile.mg5
+echo "set mneu2 ${n2}" >> ${currentFolder}currentRun/mgFile.mg5
+echo "set mch1 ${ch1}" >> ${currentFolder}currentRun/mgFile.mg5
+echo "set nevents 10000" >> ${currentFolder}currentRun/mgFile.mg5
+# echo "${outputFolder}Cards/param_card.dat" >> ${currentFolder}currentRun/mgFile.mg5
+# echo "${outputFolder}Cards/run_card.dat" >> ${currentFolder}currentRun/mgFile.mg5
 
 #log the output for post processing
 echo "running mg5_aMC ${currentFolder}currentRun/mgFile.mg5"
