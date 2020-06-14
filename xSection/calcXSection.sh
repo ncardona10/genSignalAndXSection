0c7dd723ab4f62a593412eb04b6755058dce981a
Nathalia Cardona <n.cardonac@uniandes.edu.co>
2020-06-14 16:34:23 -0400

    xsection calc functional

--- xSection/calcXSection.sh
+++ xSection/calcXSection.sh
@@ -16,8 +16,10 @@ echo Using n2 = $n2, ch1 = $ch1 and n1 = $n1
 cp ./templates/standardMgFile.mg5 ./currentRun/mgFile.mg5
 cp ./templates/standardParamCard.dat ./currentRun/param_card.dat
 
+outputFolder=../results/xSection/n2_${n2}_ch1_${ch1}_n1_${n1}/
+
 # create ouput folder
-mkdir -p ../results/xSection/n2_${n2}_ch1_${ch1}_n1_${n1} 
+mkdir -p $outputFolder 
 
 sed -i "s/outputFolder/..\/results\/xSection\/n2_${n2}_ch1_${ch1}_n1_${n1}/g" ./currentRun/mgFile.mg5
 
@@ -29,23 +31,28 @@ sed -i "s/      1000022 5.000000e+01 # mneu1/      1000022 ${n1} # mneu1/g" ./cu
 sed -i "s/      1000023 1.000000e+02 # mneu2/      1000023 ${n2} # mneu2/g" ./currentRun/param_card.dat
 sed -i "s/      1000024 7.500000e+01 # mch1/      1000024 ${ch1} # mch1/g" ./currentRun/param_card.dat
 
-cp ./currentRun/param_card.dat ../results/xSection/n2_${n2}_ch1_${ch1}_n1_${n1}/Cards/param_card.dat
-cp ./templates/run_card.dat ../results/xSection/n2_${n2}_ch1_${ch1}_n1_${n1}/Cards/run_card.dat
+cp ./currentRun/param_card.dat ${outputFolder}Cards/param_card.dat
+cp ./templates/run_card.dat ${outputFolder}Cards/run_card.dat
 
 echo "launch " >> ./currentRun/mgFile.mg5
 echo "/home/aflorez/genSignalAndXSection/results/xSection/n2_${n2}_ch1_${ch1}_n1_${n1}/Cards/param_card.dat" >> ./currentRun/mgFile.mg5
 echo "/home/aflorez/genSignalAndXSection/results/xSection/n2_${n2}_ch1_${ch1}_n1_${n1}/Cards/run_card.dat" >> ./currentRun/mgFile.mg5
 
-# mg5_aMC ./currentRun/mgFile.mg5 &> ../results/xSection/n2_${n2}_ch1_${ch1}_n1_${n1}/logXSection.txt
-
 #log the output for post processing
-script -c 'mg5_aMC ./currentRun/mgFile.mg5' -q /home/aflorez/genSignalAndXSection/results/xSection/n2_${n2}_ch1_${ch1}_n1_${n1}/logXSection.txt
+script -c 'mg5_aMC ./currentRun/mgFile.mg5' -q ./logXSection.txt
+cp ./logXSection.txt "${outputFolder}xsectionLog.txt"
 
+# get cross section values
+xsection=$(cat logXSection.txt | grep "Cross-section" | awk '{print $3}')
+xsectionerr=$(cat logXSection.txt | grep "Cross-section" | awk '{print $5}')
+nevents=$(cat logXSection.txt | grep "Nb of events" | awk '{print $5}')
 
-# change param_card_default.dat and run_card_default.dat for own files 
+echo "xsection = $xsection, xsectionerr = $xsectionerr and nevents = $nevents"
+echo "saving results to ${outputFolder}xsection.dat"
 
-# create a copy of the mg5file to an mg5launch file and include respective output 
+#write them to file
+echo "n2,ch1,n1,xsection,xsectionerr,nevents" > "${outputFolder}xsection.dat"
+echo "${n2},${ch1},${n1},${xsection},${xsectionerr},${nevents}" >> "${outputFolder}xsection.dat"
 
-# mg5_aMC mg5launchfile and log output
-
-# process log output to retrieve the data and write it to global file
+# remove temp logXsection file 
+rm ./logXSection.txt
